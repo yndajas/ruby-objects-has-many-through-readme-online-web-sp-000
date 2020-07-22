6f0c49ca71fd2d4dd9591c9e4a09f5e6eb877320
Ynda Jas <yndajas@gmail.com>
2020-07-22 14:48:32 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -28,22 +28,4 @@ class Customer
   def new_meal(waiter, total, tip=0)
     Meal.new(waiter, self, total, tip)
   end
- 
-  def new_meal_20_percent(waiter, total)
-    tip = total * 0.2
-    Meal.new(waiter, self, total, tip)
-  end
- 
-  def self.oldest_customer
-    oldest_age = 0
-    oldest_customer = nil
-    self.all.each do |customer|
-      if customer.age > oldest_age
-        oldest_age = customer.age
-        oldest_customer = customer
-      end
-    end
-    oldest_customer
-  end
- 
 end
